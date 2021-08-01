472fe026519da83efd7e745d034713dbe4fd2ca3
Tou Xiong <tlxiong.prime@outlook.com>
2021-07-31 21:17:48 -0500

    attempted strech on line 39

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -38,7 +38,7 @@ console.log('Last animal is', animalArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length,
 //      instead of the exact index number of the last item
-
+console.log('Last animal is', animalArray[animalArray.length - 1]);
 
 
 // 4. Adding & Removing Array Items
