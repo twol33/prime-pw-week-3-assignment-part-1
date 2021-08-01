c021e6bdbc0fac93eb01881b09056b6b2e2e0262
Tou Xiong <tlxiong.prime@outlook.com>
2021-07-31 21:24:45 -0500

    finished assignment to line 65

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -49,7 +49,8 @@ animalArray.push('penguin');
 console.log('Added an animal to end,', animalArray);
 
 // 4.a. TODO: Add a new food at the end of your array & log the array
-
+foodArray.push('Kapoon');
+console.log('Added a new food at the end', foodArray);
 
 
 // Example: Remove the last animal by using Array.pop
@@ -59,7 +60,9 @@ console.log('The animals are now', animalArray);
 
 // 4.b. TODO: Remove the food at the end of your array &
 //      log both the food removed and the updated array
-
+let removedFood = foodArray.pop();
+console.log('Removed the last food', removedFood);
+console.log('The favorite foods are now', foodArray);
 
 // Example: Add an animal to the beginning using Array.unshift
 animalArray.unshift('walrus');
